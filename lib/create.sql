61760cd87b7cbcc29981a789f89c3565aa81c5a0
Tiffany Pretlow <wolterpynaffit@yahoo.com>
2023-08-30 09:44:44 -0400

    finished

---
+++ lib/create.sql
@@ -0,0 +1,10 @@
+CREATE TABLE bears(
+id INTEGER PRIMARY KEY,
+name TEXT,
+age INTEGER,
+sex CHAR(1),
+color TEXT,
+temperament TEXT,
+alive BOOLEAN
+)
+
---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO bears VALUES (1, "Mr. Chocolate", 2, "M", "brown", "kind", TRUE);
+
+INSERT INTO bears VALUES (2, "Rowdy", 2, "F", "blue", "gentle", TRUE);
+
+INSERT INTO bears VALUES (3, "Tabitha", 2, "F", "white", "kind", TRUE);
+
+INSERT INTO bears VALUES (4, "Melissa", 3, "F", "black", "sweet", TRUE);
+
+INSERT INTO bears VALUES (5, "Grinch", 14, "M", "brown", "soft", TRUE);
+
+INSERT INTO bears VALUES (6,"Wendy", 2, "F", "white", "kind", TRUE);
+
+INSERT INTO bears VALUES (7, "Seargeant Brown", 2, "M", "brown", "strong", TRUE);
+
+INSERT INTO bears VALUES (8,null, 20, "M", "black", "aggressive", 0);;
